ccf7157e96a912ab0936f138387afdc8a51566a7
vlad <222vladik@gmail.com>
2020-04-02 17:46:32 +0300

    3rd step. add diff function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import genDiff from '../src/index.js';
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(genDiff(firstConfig, secondConfig));
+  })
   .parse(process.argc);
