8f6fd2d799bafde6ffa4037a296fc18adfa68529
vlad <222vladik@gmail.com>
2020-04-02 10:41:17 +0300

    delete /node_modules from commit

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,9 +2,9 @@
 
 import program from 'commander';
 
-program 
+program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]','output format')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argc);
