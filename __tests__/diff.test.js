62ce891b8d266eae5d3925ae43a7ea016d3a67ed
vlad <222vladik@gmail.com>
2020-04-03 15:29:57 +0300

    add files for test in dir __fixtures__

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -4,6 +4,6 @@ import findDiff from '../src/index.js';
 
 
 test('findDiff', () => {
-  const diffFile = fs.readFileSync(path.resolve(path.dirname('difference.json'), 'difference.json'), 'utf-8');
-  expect(findDiff('before.json', 'after.json')).toBe(diffFile);
+  const diffFile = fs.readFileSync(path.resolve(path.dirname('./__fixtures__/difference.json'), 'difference.json'), 'utf-8');
+  expect(findDiff('./__fixtures__/before.json', './__fixtures__/after.json')).toBe(diffFile);
 });
