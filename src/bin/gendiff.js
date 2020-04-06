03b8c2fcf795e0eee7a1afcc3caf12410c21670c
vlad <222vladik@gmail.com>
2020-04-06 16:41:14 +0300

    commit for test

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,7 +5,7 @@ import genDiff from '../index.js';
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.')
+  .description('Compares two configuration files and shows a differenc.')
   .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig) => {
